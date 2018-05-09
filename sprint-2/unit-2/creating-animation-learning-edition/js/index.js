c77deba59899361a3431053a397589665d12504a
Levhita <levhita@gmail.com>
2018-05-09 14:12:04 -0500

    Twoplayers

--- sprint-2/unit-2/creating-animation-learning-edition/js/index.js
+++ sprint-2/unit-2/creating-animation-learning-edition/js/index.js
@@ -1,13 +1,12 @@
 window.onload = function() {
 	let main = document.getElementsByTagName('main')[0];
-	let board = createBoard(main, 30, 20);
+	let board = createBoard(main, 60, 40);
 
-	let player=  createPlayer(2,2,"blue",board);
-	player.draw();
+	let player = createPlayer(Math.floor(board.width/2)+5,Math.floor(board.height/2),"blue",board);
 	player.direction(1,0);
-	window.board=board;
-	window.player=player;
 
+	let player2 = createPlayer(Math.floor(board.width/2)-5,Math.floor(board.height/2),"red",board);
+	player2.direction(-1,0);
 
 	document.addEventListener('keypress', function(e){
 		switch(e.key){
@@ -23,13 +22,33 @@ window.onload = function() {
 			case "ArrowRight":
 			player.direction(1,0);
 			break;
+			case "w":
+			player2.direction(0,-1);
+			break;
+			case "s":
+			player2.direction(0,1);
+			break;
+			case "a":
+			player2.direction(-1,0);
+			break;
+			case "d":
+			player2.direction(1,0);
+			break;
 		}
 	});
 
 	let step = setInterval(function(){
 		if (!player.move()) {
 			clearInterval(step);
-			alert("Crashed!");
+			alert("Crashed Blue!");
+			location.reload();
+		}
+	},100);
+
+	let step2 = setInterval(function(){
+		if (!player2.move()) {
+			clearInterval(step2);
+			alert("Crashed Red!");
 			location.reload();
 		}
 	},100);
