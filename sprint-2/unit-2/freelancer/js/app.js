1464243757e4731d257143b834f614ae95100ba1
Levhita <levhita@gmail.com>
2018-05-09 18:40:35 -0500

    pure functions

--- sprint-2/unit-2/freelancer/js/app.js
+++ sprint-2/unit-2/freelancer/js/app.js
@@ -1,25 +1,27 @@
 window.onload = function() {
-	const modal = document.getElementById('portfolioModal');
-
-	const closeButton = modal.getElementsByClassName('close')[0];
-	closeButton.addEventListener('click', function(){
-		hideModal(modal);
-	});
-
+	
 	const images = document.getElementsByClassName('portafolio-image');
 	for(i=0;i<images.length;i++){
 		images[i].addEventListener('click', function() {
-			showModal(modal, this);
+			showModal('portfolioModal', this);
 		});
 	}
+}
 
-	function showModal(modal, element) {
-		modal.getElementsByClassName('caption')[0].textContent = element.alt;
-		modal.getElementsByTagName('img')[0].src = element.src;
-		modal.style.display = "block";
-	}
+function showModal(modalId, element) {
+	const modal = document.getElementById(modalId);
 
-	function hideModal(modal) {
-		modal.style.display = "none";
-	}
+	const closeButton = modal.getElementsByClassName('close')[0];
+	closeButton.addEventListener('click', function(){
+		hideModal(modalId);
+	});
+
+	modal.getElementsByClassName('caption')[0].textContent = element.alt;
+	modal.getElementsByTagName('img')[0].src = element.src;
+	modal.style.display = "block";
+}
+
+function hideModal(modalId) {
+	const modal = document.getElementById(modalId);
+	modal.style.display = "none";
 }
