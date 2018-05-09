c9567b84a8e5274dc472c1239f3d0cb2c3a1b31b
Levhita <levhita@gmail.com>
2018-05-09 18:15:57 -0500

    final final freelancer

--- sprint-2/unit-2/freelancer/js/app.js
+++ sprint-2/unit-2/freelancer/js/app.js
@@ -1,13 +1,21 @@
 window.onload = function() {
 	const modal = document.getElementById('portfolioModal');
-	showModal(modal);
-	
+
 	const closeButton = modal.getElementsByClassName('close')[0];
 	closeButton.addEventListener('click', function(){
 		hideModal(modal);
 	});
 
-	function showModal(modal) {
+	const images = document.getElementsByClassName('portafolio-image');
+	for(i=0;i<images.length;i++){
+		images[i].addEventListener('click', function() {
+			showModal(modal, this);
+		});
+	}
+
+	function showModal(modal, element) {
+		modal.getElementsByClassName('caption')[0].textContent = element.alt;
+		modal.getElementsByTagName('img')[0].src = element.src;
 		modal.style.display = "block";
 	}
 
